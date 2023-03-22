015ca4811584df26ca66c29c5dc6acf4c9b451c9
andrei <andreirosca92@gmail.com>
2023-03-22 14:52:48 +0100

    added Link shop component

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 
 # Corso-Gatsbyjs-HPV4Learning
+
+## Introduzione a Gatsby e Installazione
+
+
+## Creare un progetto
+
+## Folder Progetto Base
+
+## Creare Routes in Gatsby
+
+## Creare 404 page
+
+## Routes Innestate
+
+## Il Link Component
--- src/pages/shop/index.js
+++ src/pages/shop/index.js
@@ -1,7 +1,22 @@
+import { Link } from 'gatsby';
 import React from 'react';
 
 const ShopHome = () => {
-  return <div>ShopHome</div>
+  return <div>
+   <h1>ShopHome</h1>
+    <nav 
+   style={{
+    display: "flex",
+    gap: "4px",
+
+   }}
+   >
+    <Link to="/">Home</Link>
+    <Link to="/shop/shoes/">Shoes</Link>
+    <Link to="/shop/socks/">Socks</Link>
+ 
+   </nav>
+  </div>
   
 }
 
